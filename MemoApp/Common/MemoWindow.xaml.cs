3721fe1a168dc65afeebb3a93b0ac60a6e6f76de
jobin jose samuel <jobin.samuel@outlook.com>
2025-04-29 17:32:11 +0530

    MemoWindow added

---
+++ MemoApp/Common/MemoWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MemoApp.Common
+{
+    /// <summary>
+    /// Interaction logic for MemoWindow.xaml
+    /// </summary>
+    public partial class MemoWindow : Window
+    {
+        public MemoWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
