b2e3cf3ca01f0627f6990bda01c3b8584ff3bbc7
jobin jose samuel <jobin.samuel@outlook.com>
2025-05-18 17:06:00 +0530

    minor updates on  save details

--- Advanced_WPF/EF/WorkersWindow.xaml.cs
+++ Advanced_WPF/EF/WorkersWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -37,8 +38,9 @@ namespace Advanced_WPF.EF
             workers_dataGrid.SelectedIndex=workers_dataGrid.Items.Count-1;
             //auto-adjust scrollbar
             //workers_dataGrid.UpdateLayout();
-            workers_dataGrid.ScrollIntoView(this.workers_dataGrid.SelectedItem);
-
+            //workers_dataGrid.ScrollIntoView(this.workers_dataGrid.SelectedItem);
+            //-------------
+            this.groupBox.IsEnabled = false;
         }
 
         void Save_Cancel_btn()
@@ -92,28 +94,125 @@ namespace Advanced_WPF.EF
 
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
+            status = "delete";
             New_Edit_Del_btn();
         }
 
-       
+        string status = "";
         private void btnAddNew_Click(object sender, RoutedEventArgs e)
         {
+            status = "new";
+
             New_Edit_Del_btn();
+            groupBox.IsEnabled = true;
+            ResetInputs();
+
+        }
+
+        void ResetInputs()
+        {
+            txt_Id.Text = "-1";
+            txt_Fname.Text = "";
+            txt_LastName.Text = "";
+            txt_Income.Text = "";
+            txt_Mobile.Text = "";
+            datePicker.Text = "";
         }
 
         private void btnEdit_Click(object sender, RoutedEventArgs e)
         {
+            status = "edit";
+            groupBox.IsEnabled = true;
             New_Edit_Del_btn();
         }
 
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
+            int savesuccess = 0;
+           
+
+            if (status == "new")
+            {
+
+
+                Worker worker = new Worker();
+                int income = 0;
+                int.TryParse(txt_Income.Text, out income);
+                worker.Income = income;
+                worker.Mobile = txt_Mobile.Text;
+                worker.Dob = DateOnly.Parse(datePicker.Text);
+                worker.Fname = txt_Fname.Text;
+                worker.Lname = txt_LastName.Text;
+                dbContext.Add(worker);
+                savesuccess = dbContext.SaveChanges();
+
+                if(savesuccess>0)
+                {
+                    workers_dataGrid.ItemsSource= dbContext.Workers.ToList();
+                    MessageBox.Show("Data added successfully");
+                    groupBox.IsEnabled = false;
+                }
+                   
+            }
+            else if (status == "edit")
+            {
+                groupBox.IsEnabled = true;
+                int _id;
+                int.TryParse(txt_Id.Text, out _id);
+                int income = 0;
+                int.TryParse(txt_Income.Text, out income);
+                Worker worker = dbContext.Workers.Where(w => w.Id == _id).FirstOrDefault();
+                if (worker != null)
+                {
+                    worker.Income = income;
+                    worker.Mobile = txt_Mobile.Text;
+                    worker.Dob = DateOnly.Parse(datePicker.Text);
+                    worker.Fname = txt_Fname.Text;
+                    worker.Lname = txt_LastName.Text;
+                    dbContext.Update(worker);
+                    savesuccess = dbContext.SaveChanges();
+                    if (savesuccess > 0)
+                    {
+                        workers_dataGrid.ItemsSource = dbContext.Workers.ToList();
+                        MessageBox.Show("Data edited successfully");
+                        groupBox.IsEnabled = false;
+                    }
+                }
+            }
+            else if (status == "delete")
+            {
+                int _id;
+                int.TryParse(txt_Id.Text, out _id);
+                Worker worker = dbContext.Workers.Where(w => w.Id == _id).FirstOrDefault();
+                if (worker != null)
+                {
+                    dbContext.Remove(worker);
+                    savesuccess = dbContext.SaveChanges();
+                    if(savesuccess>0)
+                    {
+                        workers_dataGrid.ItemsSource = dbContext.Workers.ToList();
+                        MessageBox.Show("Data deleted successfully");
+                        groupBox.IsEnabled = false;
+
+                    }
+
+                }
+            }
+
+            if(savesuccess==0)
+            {
+                MessageBox.Show("Action Failed!");
+                return;
+            }
+
             Save_Cancel_btn();
         }
 
         private void btnCancel_Click(object sender, RoutedEventArgs e)
         {
             Save_Cancel_btn();
+            groupBox.IsEnabled = false;
+            ResetInputs();
         }
     }
 }
