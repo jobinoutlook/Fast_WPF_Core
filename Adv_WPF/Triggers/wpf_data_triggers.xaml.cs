59edb41e7613acccb8df517625c7d10858406182
jobin jose samuel <jobin.samuel@outlook.com>
2025-05-24 15:58:47 +0530

    Data trigger complete

---
+++ Adv_WPF/Triggers/wpf_data_triggers.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Adv_WPF.Triggers
+{
+    /// <summary>
+    /// Interaction logic for wpf_data_triggers.xaml
+    /// </summary>
+    public partial class wpf_data_triggers : Window
+    {
+        public wpf_data_triggers()
+        {
+            InitializeComponent();
+        }
+
+        private void is_bold_chbox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
