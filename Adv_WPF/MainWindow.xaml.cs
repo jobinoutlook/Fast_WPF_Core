c7179f0abb3834330f2a5ab639f5d8ea1ee81682
jobin jose samuel <jobin.samuel@outlook.com>
2025-05-24 13:48:16 +0530

    Property triggers

---
+++ Adv_WPF/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Adv_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSet_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Resources["BtnName"] = textBox.Text;
+        }
+
+        private void btn2ndNew_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Adv_WPF/Triggers/wpf_triggers.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Adv_WPF.Triggers
+{
+    /// <summary>
+    /// Interaction logic for wpf_triggers.xaml
+    /// </summary>
+    public partial class wpf_triggers : Window
+    {
+        public wpf_triggers()
+        {
+            InitializeComponent();
+        }
+
+        private void button_1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
