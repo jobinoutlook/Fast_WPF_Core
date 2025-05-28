5cde744d83071389b41a7dfd691fc7c9a8a162ce
jobin jose samuel <jobin.samuel@outlook.com>
2025-05-28 14:52:52 +0530

    Tab Control

---
+++ Adv_WPF/Toolbar_Ribbon/wpf_StatusBar.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Adv_WPF.Toolbar_Ribbon
+{
+    /// <summary>
+    /// Interaction logic for wpf_StatusBar.xaml
+    /// </summary>
+    public partial class wpf_StatusBar : Window
+    {
+        public wpf_StatusBar()
+        {
+            InitializeComponent();
+        }
+
+        private void st_btn_info_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("I am Info Button");
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            //MessageBox.Show("I am Zoom Combobox");
+            if (cbxZoom.SelectedIndex == 0)
+            {
+                textBox.FontSize = 7;
+            }
+
+            else if (cbxZoom.SelectedIndex == 1)
+            {
+                textBox.FontSize = 10;
+            }
+            else if (cbxZoom.SelectedIndex == 2)
+            {
+                textBox.FontSize = 14;
+            }
+            else if (cbxZoom.SelectedIndex == 3)
+            {
+                textBox.FontSize = 18;
+            }
+            else if (cbxZoom.SelectedIndex == 4)
+            {
+                textBox.FontSize = 21;
+            }
+
+
+        }
+
+       
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+            lblCount.Content = textBox.Text.Length.ToString();
+        }
+    }
+}
---
+++ Adv_WPF/Toolbar_Ribbon/wpf_tab_control.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Adv_WPF.Toolbar_Ribbon
+{
+    /// <summary>
+    /// Interaction logic for wpf_tab_control.xaml
+    /// </summary>
+    public partial class wpf_tab_control : Window
+    {
+        public wpf_tab_control()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+}
